f7428722fb1e4bd6f9d54903d4da8fa19e64a262
Mateusz Dymiński <dyminski@gmail.com>
2015-02-06 16:00:11 +0100

    Backend

--- src/emiter/main.go
+++ src/emiter/main.go
@@ -6,20 +6,21 @@ import (
 	"html"
 	"net/http"
 	"time"
-
-	"github.com/mateuszdyminski/glog"
 )
 
 func main() {
+	SetupLogToKafka()
+
 	flag.Parse()
-	defer glog.Flush()
-	glog.Info("Starting")
+	Info("Starting")
 	http.HandleFunc("/", dummyHandler)
-	glog.Fatal(http.ListenAndServe(":8001", nil))
+
+	http.ListenAndServe(":8001", nil)
+
 }
 
 func dummyHandler(rw http.ResponseWriter, req *http.Request) {
-	glog.Infof("%s - - [%s] \"%s %s %s\" \"%s\" \"%s\"\n", req.RemoteAddr,
+	Infof("%s - - [%s] \"%s %s %s\" \"%s\" \"%s\"\n", req.RemoteAddr,
 		time.Now().Format("02/Jan/2006:15:04:05 -0700"), req.Method, req.RequestURI, req.Proto, req.Referer(), req.UserAgent())
 	fmt.Fprintf(rw, "Hello, %q", html.EscapeString(req.URL.Path))
 }
